45c5d8462e504d752f86260d83350e7275b210f5
cfeltz <feltzwow3@gmail.com>
2024-01-15 08:56:50 -0800

    fixing typo

--- config/channels.go
+++ config/channels.go
@@ -19,8 +19,6 @@ type Channel struct {
 	ProxyConfig      *ProxyConfig `json:"proxy"`
 	DisableTranscode bool         `json:"disableTranscode"`
 
-
-
 	// UserAgent is a custom UA string that will be used by FFMPEG to make requests to the stream URL.
 	UserAgent *string `json:"userAgent,omitempty"`
 }
